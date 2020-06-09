2e768e1ce23e9e0d689934856ce8bdb3cdabc0ab
Akshay Rangesh <arangesh@ucsd.edu>
2020-06-08 20:03:00 -0700

    Update voxelnet.py
    Cleanup.

--- det3d/models/detectors/voxelnet.py
+++ det3d/models/detectors/voxelnet.py
@@ -1,7 +1,5 @@
 from ..registry import DETECTORS
 from .single_stage import SingleStageDetector
-import torch 
-import numpy as np
 
 
 @DETECTORS.register_module
@@ -23,11 +21,7 @@ class VoxelNet(SingleStageDetector):
     def extract_feat(self, data):
         input_features = self.reader(data["features"], data["num_voxels"])
         x, points = self.backbone(input_features, data["coors"], data["batch_size"], data["input_shape"])
-#         x = self.backbone(input_features, data["coors"], data["batch_size"], data["input_shape"])
-                
-#         x = self.backbone(
-#             input_features, data["coors"], data["batch_size"], data["input_shape"]
-#         )
+
         if self.with_neck:
             x = self.neck(x)
         return x, points
@@ -37,18 +31,8 @@ class VoxelNet(SingleStageDetector):
         coordinates = example["coordinates"]
         num_points_in_voxel = example["num_points"]
         num_voxels = example["num_voxels"]
-        
-#         for batch_size in range(len(example["annos"])):
-#             for i in range(6):
-#                 print("example annos",example["annos"][batch_size]["gt_boxes"][i].shape)
-#             print("REG TARGETS LAST 5", example["reg_targets"][i][0,:-5,:])
-        batch_size = len(num_voxels)
-#         for i in range(6):
-#             print("EXAMPLE0!!!",len(example["labels"][i][0]==0))
-#             print("EXAMPLE-1!!!",len(example["labels"][i][0]==-1))
-#             print("EXAMPLE1!!!",len(example["labels"][i][0]==1))
-#             print("EXAMPLE2!!!",len(example["labels"][i][0]==2))
-                        
+
+        batch_size = len(num_voxels)                      
 
         data = dict(
             features=voxels,
